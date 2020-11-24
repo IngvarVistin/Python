58d38cecafaf1bf044dbe97a88ad49966a97e52c
IngvarVistin <74676896+IngvarVistin@users.noreply.github.com>
2020-11-24 19:07:48 +0900

    Add files via upload

---
+++ Sec_on_time.py
@@ -0,0 +1,6 @@
+time_sec = int(input('введите время в секундах '))
+hour = int(time_sec/3600)
+min = int((time_sec-(hour*3600))/60)
+sec = time_sec-(hour*3600)-(min*60)
+time_str='{}:{}:{}'.format(hour,min,sec)
+print(time_str)
